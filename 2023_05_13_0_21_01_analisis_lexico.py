f56a487ecd682cea3d81203d561ee337f64829b8
Alnitakr51 <gadieljj34@gmail.com>
2023-05-14 02:36:33 +0200

    Tratamiento Lógico del Lenguaje

---
+++ 2023_05_13_0_21_01_analisis_lexico.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun May 14 01:40:22 2023
+
+@author: Gadiel Jimenez
+"""
+
+
+
+from sklearn.feature_extraction.text import TfidfVectorizer
+
+# lista de preguntas
+preguntas = ["¿Que es la Inteligencia Artificial?"]
+
+# Crea el objeto de vectorizador TfidfVectorizer
+vectorizer = TfidfVectorizer()
+
+# Convierte el texto en una matriz de características numericas basada en la
+# frecuencia de los términos y la importancia inversa del documento (TF-IDF).
+vector_preguntas = vectorizer.fit_transform(preguntas)
+
+# imprimir la matriz de pesos
+print("\n", vector_preguntas.toarray())
+
+# imprimir la lista de palabras en el orden de los pesos
+print("\n", vectorizer.get_feature_names_out())
